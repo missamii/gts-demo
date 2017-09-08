035bc937434e301d31c6b12df3553012b48eaf2c
Tiffany <tiffany.m.moore.nyc@gmail.com>
2017-09-08 14:08:14 -0400

    Add files via upload

---
+++ dropdown.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+  console.log("DOM Loaded"); // DOM and js file check
+
+  //nav bar ui dropdown controls
+  $('.ui.dropdown')
+  .dropdown();
+  
+}; //end DOM and js file check
